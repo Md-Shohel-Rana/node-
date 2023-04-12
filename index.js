42dda43d36b3977308a11df42c631553fbf2b06c
Md Shohel Rana <113632658+Md-Shohel-Rana@users.noreply.github.com>
2023-04-12 22:26:56 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const {getName} = require('./student');
+console.log(getName());
+
+// IMPORT MODULES HERE
+// const s1= require('./student');
+// console.log(s1.getAge());
+// console.log(s1.getName());
+// console.log(s1.getCgp());
+// console.log(s1.gpa);
+
---
+++ student.js
@@ -0,0 +1,29 @@
+exports.getName = ()=>{
+    return("developer");
+}
+
+const getAge = ()=>{
+    return(20);
+}
+const getCgp = ()=>{
+    return(4.00)
+}
+
+const gpa = 3.38;
+
+// EXPORTS MODULES
+// exports.name=getName;
+// exports.age=getAge;
+// exports.result=getCgp;
+// exports.result1=gpa;
+
+// module.exports = {
+
+//  getName,
+//  getAge,
+//  getCgp,
+//  gpa
+
+// }
+
+
